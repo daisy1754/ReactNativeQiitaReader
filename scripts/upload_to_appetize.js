c51301612cc20eeb334c2b329eef938f0ea8fac1
daisy1754 <d-part@hotmail.co.jp>
2017-12-11 20:48:54 -0800

    Add script to upload the latest apk file to appetize.io

---
+++ scripts/upload_to_appetize.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+const fs = require('fs');
+const FormData = require('form-data');
+const request = require('request');
+
+const args = process.argv.slice(2);
+if (args.length == 0 || args[0].length == 0) {
+  throw new Error("APPETIZE_API_TOKEN must be set");
+}
+
+console.log("Uploading apk...");
+request.post({
+  url: `https://${args[0]}@api.appetize.io/v1/apps`,
+  formData:  {
+    platform: 'android',
+    file: fs.createReadStream('android/app/build/outputs/apk/app-release.apk'),
+  }
+}, function (err, res, body) {
+  if (err) {
+    throw new Error("Failed to upload apk", err);
+  } else {
+    console.log(`App is succeesfully uploaded!`);
+    console.log(JSON.parse(body).publicURL);
+  }
+});
